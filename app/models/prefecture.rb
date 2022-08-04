840bcf0e25c1e90fc775635bf576b5b6c21a8107
Issei.M <issei.m7@gmail.com>
2022-08-04 17:01:08 +0900

    Add more detailed relation settings to Prefecture

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,2 +1,9 @@
 class Prefecture < ApplicationRecord
+  has_many :people_living_here, class_name: :Person
+  has_many :prefectures_person_wants_to_live_in, class_name: :PrefecturePersonWantsToLiveIn
+  has_many :people_wanting_to_live_here, through: :prefectures_person_wants_to_live_in, source: :person
+
+  def self.tokyo
+    find(13)
+  end
 end
