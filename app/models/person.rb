8e5c0b378188815df554bfefdbf5f592d453f641
Issei.M <issei.m7@gmail.com>
2022-08-06 19:55:41 +0900

    Add `dependant: :destroy` to Person.prefectures_person_wants_to_live_in

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ApplicationRecord
   belongs_to :live_in, class_name: :Prefecture, foreign_key: :prefecture_id
-  has_many :prefectures_person_wants_to_live_in, class_name: :PrefecturePersonWantsToLiveIn
+  has_many :prefectures_person_wants_to_live_in, class_name: :PrefecturePersonWantsToLiveIn, dependent: :destroy
   has_many :prefectures_to_want_to_live_in, through: :prefectures_person_wants_to_live_in, source: :prefecture
 
   enum sex: [ :male, :female, :other ]
